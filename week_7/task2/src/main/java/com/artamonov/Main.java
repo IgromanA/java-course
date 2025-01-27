b914a58a9c5514733f36759a5c8a7e4753f2225f
IgromanA <artemii200588@gmail.com>
2025-01-27 12:57:38 +0300

    Выполнение задания 7

---
+++ week_7/task2/src/main/java/com/artamonov/Main.java
@@ -0,0 +1,25 @@
+package com.artamonov;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Укажите путь к файлу заказов как аргумент командной строки.");
+            return;
+        }
+
+        String filePath = args[0];
+        OrderCompiler generator = new OrderCompiler();
+        List<Order> orders = generator.compileOrders(filePath);
+
+        double totalAmount = orders.stream()
+            .filter(order -> order.getOrderDate().getYear() == 2020 &&
+                             order.getOrderDate().getMonthValue() == 2)
+            .flatMap(order -> order.getProducts().stream())
+            .mapToDouble(Product::getPrice)
+            .sum();
+
+        System.out.println("Общая сумма заказов в феврале 2020 г.: " + totalAmount);
+    }
+}
