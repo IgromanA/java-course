a512176f4840fd47eebcc33c8faffa06cb7d171d
IgromanA <artemii200588@gmail.com>
2025-01-29 04:11:23 +0300

    Выполнение задания 9 недели

---
+++ week_9/task_gradle/src/main/java/com/artamonov/OptimizedFibonacci.java
@@ -0,0 +1,23 @@
+package com.artamonov;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class OptimizedFibonacci {
+
+    private static Map<Integer, Long> cache = new HashMap<>();
+
+    public static long cachedFibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+
+        if (cache.containsKey(n)) {
+            return cache.get(n);
+        }
+
+        long result = cachedFibonacci(n - 1) + cachedFibonacci(n - 2);
+        cache.put(n, result);
+        return result;
+    }
+}
